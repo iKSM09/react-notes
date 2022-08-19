85f5ef6feb7b55192db5d6233f31606904aad9df
Kunal Sharma <kunal.iksm09@gmail.com>
2022-08-19 23:11:12 +0530

    added complete and deleting functionality

--- src/routes/TodoApp.component.jsx
+++ src/routes/TodoApp.component.jsx
@@ -1,5 +1,12 @@
 import { useState, useEffect } from "react";
-import { MdAddBox } from "react-icons/md";
+import {
+  MdAddBox,
+  MdAddCircleOutline,
+  MdSubdirectoryArrowRight,
+  MdTaskAlt,
+  MdRadioButtonUnchecked,
+  MdDelete,
+} from "react-icons/md";
 import styled from "styled-components";
 
 const InputContainer = styled.section`
@@ -25,6 +32,49 @@ const TaskInputContainer = ({ addNewTaskHandler }) => {
   );
 };
 
+const TodoContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-block: 4px;
+`;
+
+const TaskWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-block: 4px;
+`;
+
+const Task = styled.p`
+  margin: 0;
+  font-size: 16px;
+  text-decoration: ${({ completed }) => (completed ? "line-through" : "none")};
+`;
+
+const TodoItem = ({
+  todo: { id, task, completed, editable },
+  completeTaskHandler,
+  deleteTaskHandler,
+}) => {
+  return (
+    <TodoContainer>
+      <TaskWrapper>
+        <span onClick={() => completeTaskHandler(id)}>
+          {completed ? (
+            <MdTaskAlt style={{ fontSize: "18px", margin: "6px" }} />
+          ) : (
+            <MdRadioButtonUnchecked
+              style={{ fontSize: "18px", margin: "6px" }}
+            />
+          )}
+        </span>
+        <Task completed={completed}>{task}</Task>
+      </TaskWrapper>
+      <MdDelete onClick={() => deleteTaskHandler(id)} />
+    </TodoContainer>
+  );
+};
+
 const TodoApp = () => {
   const [todoList, setTodoList] = useState([
     { id: "01", task: "Learn Html", completed: false, editable: false },
@@ -34,7 +84,6 @@ const TodoApp = () => {
 
   const addNewTask = (e) => {
     if (e.key === "Enter") {
-      console.log(e.target.value);
       const newTodo = {
         id: new Date().getTime().toString(),
         task: e.target.value,
@@ -46,17 +95,32 @@ const TodoApp = () => {
     }
   };
 
-  const completeTask = () => {};
+  const completeTask = (id) => {
+    const completedTodo = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    setTodoList(completedTodo);
+  };
 
-  const deleteTask = () => {};
+  const deleteTask = (id) => {
+    console.log("deleted", id);
+    const updatedTodoList = todoList.filter((todo) => todo.id !== id);
+    setTodoList(updatedTodoList);
+  };
 
   return (
     <div>
       <h2>My Tasks</h2>
       {todoList.map((todo) => (
-        <div key={todo.id}>
-          <p>{todo.task}</p>
-        </div>
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          completeTaskHandler={completeTask}
+          deleteTaskHandler={deleteTask}
+        />
       ))}
       <TaskInputContainer addNewTaskHandler={addNewTask} />
     </div>
