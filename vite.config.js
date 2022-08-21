2e1bd61da1f80e407f843e26d63ad98e3a4d1468
Kunal Sharma <kunal.iksm09@gmail.com>
2022-08-22 00:38:07 +0530

    changed build target to es2019

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: ["es2019", "edge88", "firefox78", "chrome87", "safari13.1"],
+    polyfillModulePreload: true,
+  },
   plugins: [react()],
 });
